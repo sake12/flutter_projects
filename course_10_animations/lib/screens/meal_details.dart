543546a7ca3e60056ba0ecae9711bc90279b82bd
sake12 <sake12@gmail.com>
2024-02-19 23:22:50 +0100

    implicit animation

--- course_10_animations/lib/screens/meal_details.dart
+++ course_10_animations/lib/screens/meal_details.dart
@@ -33,7 +33,19 @@ class MealDetailsScreen extends ConsumerWidget {
                 ),
               );
             },
-            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
+            icon: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 500),
+              transitionBuilder: (child, animation) {
+                return RotationTransition(
+                  turns: Tween(begin: 0.8, end: 1.0).animate(animation),
+                  child: child,
+                );
+              },
+              child: Icon(
+                isFavorite ? Icons.star : Icons.star_border,
+                key: ValueKey(isFavorite),
+              ),
+            ),
           )
         ]),
         body: SingleChildScrollView(
